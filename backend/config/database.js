96de10f764b923622b43a2f488c1d312b3cfc0f6
unknown <sundp.kshetri@gmail.com>
2021-08-21 16:46:26 +0545

    connecting to the database

---
+++ backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(
+    "mongodb://localhost:27017/consultDoctor",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  );
+
+  console.log("Mongo Db connected:" + conn.connection.host);
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const app = require("./app");
-
+const connectDatabase = require("./config/database");
 const dotenv = require("dotenv");
 
 //Setting up config file
 dotenv.config({ path: "backend/config/config.env" });
+
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(
